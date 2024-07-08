602347e2d88e08679d7608790461e0f70509f72d
Aymen-Khanfir <aymenkhanfir56@gmail.com>
2024-07-08 13:57:56 +0100

    Ajusting button styling

--- lib/common/widgets/button/basic_app_button.dart
+++ lib/common/widgets/button/basic_app_button.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class BasicAppButton extends StatelessWidget {
   const BasicAppButton({super.key, required this.onPressed, required this.title, this.height});
-
   final VoidCallback onPressed;
   final String title;
   final double? height;
@@ -10,9 +9,12 @@ class BasicAppButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
-      style: ElevatedButton.styleFrom(maximumSize: Size.fromHeight(height ?? 80)),
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size.fromHeight(height ?? 80),
+      ),
       child: Text(
+        style: const TextStyle(color: Colors.white),
         title,
       ),
     );
